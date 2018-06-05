17f9349dbe991ac55bfead74ee9bc1998c64232b
Makiuchi Keiichiro <makiuchi.k1low@gmail.com>
2018-06-05 17:59:41 +0900

    add .gitconfig

--- mklink.bat
+++ mklink.bat
@@ -1,4 +1,7 @@
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
 mklink %HOMEPATH%"\.gvimrc" %HOMEPATH%"\dotfiles\.gvimrc"
 mklink /D %HOMEPATH%"\.vim" %HOMEPATH%"\dotfiles\.vim"
+
+mklink %HOMEPATH%"\.gitconfig" %HOMEPATH%"\dotfiles\.gitconfig"
+
 exit 0
