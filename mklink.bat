bc201f11d79a600c264a28e8fab865115feb35e8
Makiuchi Keiichiro <makiuchi.k1low@gmail.com>
2018-06-06 18:20:54 +0900

    add .NERDTreeBookmarks

--- mklink.bat
+++ mklink.bat
@@ -1,5 +1,6 @@
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
 mklink %HOMEPATH%"\.gvimrc" %HOMEPATH%"\dotfiles\.gvimrc"
+mklink %HOMEPATH%"\.NERDTreeBookmarks" %HOMEPATH%"\dotfiles\.vim\.NERDTreeBookmarks"
 mklink /D %HOMEPATH%"\.vim" %HOMEPATH%"\dotfiles\.vim"
 
 mklink %HOMEPATH%"\.gitconfig" %HOMEPATH%"\dotfiles\.gitconfig"
