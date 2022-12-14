27e4ec1fc6b7a620b192012599dd18f871d78df9
Makiuchi Keiichiro <makiuchi.k1low@gmail.com>
2022-12-14 11:02:21 +0900

    .bashrc追加

--- mklink.bat
+++ mklink.bat
@@ -1,9 +1,12 @@
+rem vimŠÖ˜A
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
 mklink %HOMEPATH%"\.gvimrc" %HOMEPATH%"\dotfiles\.gvimrc"
 mklink %HOMEPATH%"\.NERDTreeBookmarks" %HOMEPATH%"\dotfiles\.vim\.NERDTreeBookmarks"
 mklink /D %HOMEPATH%"\.vim" %HOMEPATH%"\dotfiles\.vim"
 
+rem git,SSH
 mklink %HOMEPATH%"\.gitconfig" %HOMEPATH%"\dotfiles\.gitconfig"
 mklink /D %HOMEPATH%"\.ssh" %HOMEPATH%"\dotfiles\.ssh"
 
-rem exit 0
+rem bash
+mklink %HOMEPATH%"\.bashrc" %HOMEPATH%"\dotfiles\.bashrc"
