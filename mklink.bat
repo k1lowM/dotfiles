b77b08deb7eca4a672bfc8723cdaa9b2a708618f
Makiuchi Keiichiro <makiuchi.k1low@gmail.com>
2023-02-08 13:46:30 +0900

    .sshの管理削除

--- mklink.bat
+++ mklink.bat
@@ -4,9 +4,8 @@ mklink %HOMEPATH%"\.gvimrc" %HOMEPATH%"\dotfiles\.gvimrc"
 mklink %HOMEPATH%"\.NERDTreeBookmarks" %HOMEPATH%"\dotfiles\.vim\.NERDTreeBookmarks"
 mklink /D %HOMEPATH%"\.vim" %HOMEPATH%"\dotfiles\.vim"
 
-rem git,SSH
+rem git
 mklink %HOMEPATH%"\.gitconfig" %HOMEPATH%"\dotfiles\.gitconfig"
-mklink /D %HOMEPATH%"\.ssh" %HOMEPATH%"\dotfiles\.ssh"
 
 rem bash
 mklink %HOMEPATH%"\.bashrc" %HOMEPATH%"\dotfiles\.bashrc"
